6b40c9d43dbb6589f24525265915f0f4526498b5
Jonas Røssum <jonaswehoo@gmail.com>
2019-09-11 13:55:13 +0200

    close input stream

--- TicTacToe.java
+++ TicTacToe.java
@@ -106,6 +106,8 @@ class TicTacToe {
       board.outputBoard();
     } catch (InputMismatchException e) {
       System.out.println("Invalid move.");
+    } finally {
+      s.close();
     }
   }
 
